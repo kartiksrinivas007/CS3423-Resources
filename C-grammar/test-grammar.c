c8d2e381e9921cdf94cbdf27bfe5cd3796b9be88
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-08-31 11:29:27 +0530

    Grammar Modified

--- C-grammar/test-grammar.c
+++ C-grammar/test-grammar.c
@@ -4,6 +4,13 @@ int a = 5;
 int main(){
 	int x , y, z; 
 	x = 2, y  = 3, z  = 4;
+	int arr[3] = {x = y++, 0,}; //{initializer_list}
+	printf("%d", arr[0]);
 	printf("Value ofz = %d",z);
+	if( x > 3){
+	{
+		printf("lol");
+	}	
+	}
 	return 0;
 }
